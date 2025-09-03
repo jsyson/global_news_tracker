09a796381cef4afffedbb2a6737db36e97ebb317
1108775 <jason_jung@sk.com>
2025-09-03 14:58:58 +0900

    selenium 옵션 수정4 - ua 설정 제거

--- get_downdetector_web.py
+++ get_downdetector_web.py
@@ -81,7 +81,6 @@ options.add_argument("--window-size=1920,1080")
 options.add_argument("--disable-gpu")
 options.add_argument("--headless")
 # options.add_argument("--headless=new")  # 최신 헤드리스 모드를 사용
-options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36')
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-blink-features=AutomationControlled")
